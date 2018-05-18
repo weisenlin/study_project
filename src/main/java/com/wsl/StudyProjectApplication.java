652948f44100dedd206e0d000933adde9225013b
weisenlin <1209867203@qq.com>
2018-05-18 11:22:58 +0800

    学习计划项目

---
+++ src/main/java/com/wsl/StudyProjectApplication.java
@@ -0,0 +1,12 @@
+package com.wsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyProjectApplication.class, args);
+	}
+}
